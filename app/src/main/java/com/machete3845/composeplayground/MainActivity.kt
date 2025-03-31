98af4d08979e5779a858bd75dfb82f2f745d01b7
machete38 <machete384@icloud.com>
2025-03-31 19:29:10 +0300

    Базовое состояние

--- app/src/main/java/com/machete3845/composeplayground/MainActivity.kt
+++ app/src/main/java/com/machete3845/composeplayground/MainActivity.kt
@@ -1,14 +1,23 @@
 package com.machete3845.composeplayground
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
@@ -51,12 +60,29 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 fun GreetingPreview() {
     ComposePlaygroundTheme {
         Scaffold { innerPadding ->
-            Greeting(
-                "Android",
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(innerPadding)
-            )
+            CounterScreen(modifier = Modifier.padding(innerPadding))
         }
     }
 }
+
+@Composable
+fun CounterScreen(modifier: Modifier = Modifier){
+
+    var counter by rememberSaveable { mutableStateOf(0) }
+
+    Column(modifier = Modifier.fillMaxSize()){
+        Text(text = "Counter $counter")
+        Button(onClick = { counter++}){
+            Text("Increment")
+        }
+    }
+
+    DisposableEffect(Unit) {
+        Log.d("CounterScreen", "Screen entered")
+
+        onDispose {
+            Log.d("CounterScreen", "Screen exited")
+        }
+    }
+}
+
