258cbccc05d8db75782d7a2a61f7b7e3995ca741
machete38 <machete384@icloud.com>
2025-04-04 21:35:17 +0300

    Сложный экран ч. 3

--- app/src/main/java/com/machete3845/composeplayground/MainActivity.kt
+++ app/src/main/java/com/machete3845/composeplayground/MainActivity.kt
@@ -8,10 +8,13 @@ import androidx.annotation.Dimension
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
@@ -42,7 +45,10 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposePlaygroundTheme {
-                ConstraintLayoutExample()
+                Column {
+                    ConstraintLayoutExample()
+                    CombinedExample()
+                }
             }
         }
     }
@@ -162,6 +168,26 @@ fun CounterScreen(modifier: Modifier = Modifier){
 
         onDispose {
             Log.d("CounterScreen", "Screen exited")
+        }
+    }
+
+}
+
+@Composable
+fun CombinedExample() {
+    Row(modifier = Modifier.fillMaxWidth()) {
+        Button(
+            onClick = { /* действие */ },
+            modifier = Modifier.weight(1f)
+        ) {
+            Text("Левая")
+        }
+        Spacer(modifier = Modifier.width(16.dp))
+        Button(
+            onClick = { /* действие */ },
+            modifier = Modifier.weight(1f)
+        ) {
+            Text("Правая")
         }
     }
 }
