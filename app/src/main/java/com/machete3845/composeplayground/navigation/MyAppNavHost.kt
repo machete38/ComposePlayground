a37b1c5049a2fb54dbef6d988c8b988d6f0db44f
machete38 <machete384@icloud.com>
2025-04-07 13:19:17 +0300

    Навигация

--- app/src/main/java/com/machete3845/composeplayground/navigation/MyAppNavHost.kt
+++ app/src/main/java/com/machete3845/composeplayground/navigation/MyAppNavHost.kt
@@ -1,10 +1,55 @@
-package com.machete3845.composeplayground
+package com.machete3845.composeplayground.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.machete3845.composeplayground.navigation.NavRoutes
+import com.machete3845.composeplayground.screens.DetailsScreen
+import com.machete3845.composeplayground.screens.HomeScreen
+import com.machete3845.composeplayground.screens.ProfileScreen
 
 @Composable
 fun MyAppNavHost(
-    navController: NavHostController = rememberNavHost
-){
-
+    navController: NavHostController = rememberNavController(),
+    startDestination: String = NavRoutes.HOME
+) {
+    NavHost(
+        navController = navController,
+        startDestination = startDestination
+    ) {
+        composable(NavRoutes.HOME) {
+            HomeScreen(
+                onNavigateToDetails = { navController.navigate(NavRoutes.DETAILS) }
+            )
+        }
+        composable(
+            route = NavRoutes.DETAILS+"/{itemId}?name={name}",
+            arguments = listOf(
+                navArgument("itemId") { type = NavType.IntType },
+                navArgument("name") {
+                    type = NavType.StringType
+                    nullable = true
+                    defaultValue = null
+                }
+            )
+        ) { backStackEntry ->
+            val itemId = backStackEntry.arguments?.getInt("itemId") ?: 0
+            val name = backStackEntry.arguments?.getString("name")
+            DetailsScreen(
+                onNavigateUp = { navController.navigateUp() },
+                onNavigateToProfile = { navController.navigate(NavRoutes.PROFILE) },
+                itemId = itemId,
+                name = name
+            )
+        }
+        composable(NavRoutes.PROFILE) {
+            ProfileScreen(
+                onNavigateUp = { navController.navigateUp() }
+            )
+        }
+    }
 }
--- app/src/main/java/com/machete3845/composeplayground/navigation/NavRoutes.kt
+++ app/src/main/java/com/machete3845/composeplayground/navigation/NavRoutes.kt
@@ -1,2 +1,7 @@
-package com.machete3845.composeplayground.navigation 
+package com.machete3845.composeplayground.navigation
 
+object NavRoutes {
+    const val HOME = "Домашняя страница"
+    const val DETAILS = "Детали"
+    const val PROFILE = "Профиль"
+}
