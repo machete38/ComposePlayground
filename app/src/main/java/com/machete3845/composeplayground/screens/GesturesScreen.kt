0803409cdea5b7582747fadcf48bdabbc8f7221a
machete38 <machete384@icloud.com>
2025-04-21 22:39:39 +0300

    Жесты

--- app/src/main/java/com/machete3845/composeplayground/screens/GesturesScreen.kt
+++ app/src/main/java/com/machete3845/composeplayground/screens/GesturesScreen.kt
@@ -5,11 +5,16 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.gestures.detectTransformGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -19,7 +24,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
@@ -32,7 +39,11 @@ fun GesturesScreen(){
         ClickableExample()
         CombinedClickable()
         Text("Обработка сложных жестов с помощью PointerInputScope")
-        DragExample()
+//        DragExample()
+//        TransformableExample()
+        Text("Использование готовых модификаторов для жестов")
+        ScrollableExample()
+        SwipeableExample()
     }
 }
 
@@ -107,4 +118,76 @@ fun DragExample(){
             )
         }
     }
+}
+
+@Composable
+fun TransformableExample(){
+    var scale by remember{ mutableFloatStateOf(1f) }
+    var rotation by remember { mutableFloatStateOf(0f)}
+    var offset by remember { mutableStateOf(Offset.Zero) }
+
+    Column {
+        Text("Обнаружение жестов масштабирования и поворота")
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        )
+        {
+            Box(
+                Modifier
+                    .graphicsLayer(
+                        scaleX = scale,
+                        scaleY = scale,
+                        rotationZ = rotation,
+                        translationX = offset.x,
+                        translationY = offset.y
+                    )
+                    .size(200.dp)
+                    .background(Color.Blue)
+                    .pointerInput(Unit){
+                        detectTransformGestures { _, pan, zoom, rotationChange ->
+                            scale *= zoom
+                            rotation += rotationChange
+                            offset += pan
+                        }
+                    }
+            ){
+
+            }
+        }
+    }
+}
+
+
+// Использование готовых модификаторов для жестов
+@Composable
+fun ScrollableExample(){
+    val scrollState = rememberScrollState()
+    Column {
+        Text("Модификатор Scrollable")
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .verticalScroll(scrollState)
+                .padding(16.dp)
+        ){
+            repeat(50) {
+                index ->
+                Text(
+                    text = "Item $index",
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp)
+                )
+            }
+        }
+    }
+
+}
+
+@Composable
+fun SwipeableExample(){
+    val width = 96.dp
+    val squareSize = 48.dp
+
 }
