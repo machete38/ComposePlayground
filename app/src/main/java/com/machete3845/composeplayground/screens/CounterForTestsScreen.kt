29c81ea94e930a4de14100771ba88cfa7dfaeb3a
machete38 <machete384@icloud.com>
2025-04-24 23:43:08 +0300

    Тесты

--- app/src/main/java/com/machete3845/composeplayground/screens/CounterForTestsScreen.kt
+++ app/src/main/java/com/machete3845/composeplayground/screens/CounterForTestsScreen.kt
@@ -6,11 +6,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 
-@Composable
-fun Counter() {
-    val count = remember { mutableStateOf(0) }
-
-    Button(onClick = { count.value++ }) {
-        Text("Count: ${count.value}")
-    }
-}
+//@Composable
+//fun Counter() {
+//    val count = remember { mutableStateOf(0) }
+//
+//    Button(onClick = { count.value++ }) {
+//        Text("Count: ${count.value}")
+//    }
+//}
