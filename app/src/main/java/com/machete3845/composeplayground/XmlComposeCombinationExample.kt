9a683e86ff26a4f8a922252ee2b29c35da0c8a8b
machete38 <machete384@icloud.com>
2025-04-29 21:18:13 +0300

    ComposeView в XML (часть 1)

---
+++ app/src/main/java/com/machete3845/composeplayground/XmlComposeCombinationExample.kt
@@ -0,0 +1,20 @@
+package com.machete3845.composeplayground
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class XmlComposeCombinationExample : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_xml_compose_combination_example)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
