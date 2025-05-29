e294f0ccad702277dcbe4cb80e62eecfeab5cce3
machete38 <machete384@icloud.com>
2025-05-29 18:30:35 +0300

    Path (canvas)

--- features/composeCanvas/src/main/java/com/machete3845/composecanvas/CanvasScreen.kt
+++ features/composeCanvas/src/main/java/com/machete3845/composecanvas/CanvasScreen.kt
@@ -9,6 +9,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.StrokeJoin
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.unit.dp
 
@@ -22,13 +25,65 @@ private fun Screen(){
     Column{
         Text("Для начала работы с Canvas используется компонент Canvas")
         BasicCanvasCircle()
-        Text("Canvas позволяет рисовать различные примитивы:")
+        Text("Canvas позволяет рисовать различные примитивы")
         ShapesCanvas()
+        Text("Для создания сложных фигур используется Path")
+        PathCanvas()
+        Text("Canvas позволяет применять градиенты и различные эффекты")
     }
 }
 
 @Composable
-fun ShapesCanvas() {
+private fun PathCanvas() {
+    Canvas(Modifier.size(300.dp)) {
+        val path = Path().apply {
+            // Начинаем с верхней точки
+            moveTo(150f, 50f)
+
+            // Рисуем пять линий для создания звезды
+            lineTo(100f, 180f)
+            lineTo(200f, 100f)
+            lineTo(100f, 100f)
+            lineTo(200f, 180f)
+
+            // Замыкаем путь
+            close()
+        }
+
+        // Рисуем путь с заливкой
+        drawPath(
+            path = path,
+            color = Color.Yellow
+        )
+
+        // Рисуем контур пути
+        drawPath(
+            path = path,
+            color = Color.Black,
+            style = Stroke(
+                width = 3f,
+                cap = StrokeCap.Round,
+                join = StrokeJoin.Round
+            )
+        )
+
+        val bezierPath = Path().apply {
+            moveTo(50f, 250f)
+            // Кубическая кривая
+
+            cubicTo(x1 = 100f, y1 = 200f, x2 =  200f, y2 = 300f, x3 = 250f, y3 = 250f)
+        }
+
+        drawPath(
+            path = bezierPath,
+            color = Color.Blue,
+            style = Stroke(width = 5f)
+        )
+    }
+}
+
+@Composable
+private fun ShapesCanvas() {
    Canvas(Modifier.size(300.dp))
    {
        // Линия
@@ -75,7 +130,7 @@ fun ShapesCanvas() {
 }
 
 @Composable
-fun BasicCanvasCircle() {
+private fun BasicCanvasCircle() {
     Canvas(Modifier.size(100.dp)){ // Size содержит ширину и высоту Canvas
         // Рисуем круг в центре Canvas
         drawCircle(
